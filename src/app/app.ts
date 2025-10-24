37d5330570631861283effd7b464fd12956717c4
omokhepe <fimobu@gmail.com>
2025-10-24 19:49:23 +0800

    First Push

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,23 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {FooterComponent} from './components/footer/footer.component';
+import {HeaderComponent} from './components/header/header.component';
+import AOS from 'aos';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FooterComponent, HeaderComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit {
+  ngOnInit() {
+      AOS.init({
+        duration: 800,
+        once: false,
+        mirror: true,
+      });
+  }
+
   protected readonly title = signal('school-site');
 }
